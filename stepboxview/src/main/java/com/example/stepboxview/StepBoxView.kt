1a827caa2babcd207be8a3ba2ab1c83866b091dc
Anwesh43 <anweshthecool0@gmail.com>
2018-05-07 09:37:57 +0530

    adding StepBoxView class

--- stepboxview/src/main/java/com/example/stepboxview/StepBoxView.kt
+++ stepboxview/src/main/java/com/example/stepboxview/StepBoxView.kt
@@ -8,3 +8,21 @@ import android.content.*
 import android.view.*
 import android.graphics.*
 
+class StepBoxView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
